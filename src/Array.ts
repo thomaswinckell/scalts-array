2acbb3e3a90108ce2821193ad4f8a6e8cdd2ad58
Thomas Winckell <thomas.winckell@gmail.com>
2016-11-15 22:56:59 +0100

    Intersection, intersectionBy, intersectionWith, isEmpty, last, lastIndexOfOpt

--- src/Array.ts
+++ src/Array.ts
@@ -31,22 +31,14 @@ declare global {
          * The order and references of result values are determined by the first array.
          * The comparator is invoked with two arguments: (arrVal, othVal).
          */
-        differenceBy(...values : Array<T>) : Array<T>;
-        differenceBy(value : Array<T>, iteratee : (value:T) => any|string) : Array<T>;
-        differenceBy(value1 : Array<T>, value2 : Array<T>, iteratee : (value:T) => any|string) : Array<T>;
-        differenceBy(value1 : Array<T>, value2 : Array<T>, value3 : Array<T>, iteratee : (value:T) => any|string) : Array<T>;
-        differenceBy(value1 : Array<T>, value2 : Array<T>, value3 : Array<T>, value4 : Array<T>, iteratee : (value:T) => any|string) : Array<T>;
+        differenceBy(values : Array<Array<T>>, iteratee : ((value:T) => any)|string) : Array<T>;
 
         /**
          * This method is like difference except that it accepts comparator which is invoked to compare elements of array to values.
          * The order and references of result values are determined by the first array.
          * The comparator is invoked with two arguments: (arrVal, othVal).
          */
-        differenceWith(...values : Array<T>) : Array<T>;
-        differenceWith(value : Array<T>, comparator : (value1:T, value2:T) => boolean) : Array<T>;
-        differenceWith(value1 : Array<T>, value2 : Array<T>, comparator : (value1:T, value2:T) => boolean) : Array<T>;
-        differenceWith(value1 : Array<T>, value2 : Array<T>, value3 : Array<T>, comparator : (value1:T, value2:T) => boolean) : Array<T>;
-        differenceWith(value1 : Array<T>, value2 : Array<T>, value3 : Array<T>, value4 : Array<T>, comparator : (value1:T, value2:T) => boolean) : Array<T>;
+        differenceWith(values : Array<Array<T>>, comparator : (value1:T, value2:T) => boolean) : Array<T>;
 
         /**
          * Creates a slice of array with n elements dropped from the beginning.
@@ -107,19 +99,53 @@ declare global {
         flattenDepth<U>(depth ?: number) : Array<U>;
 
         /**
-         * Returns optionally the first element of the array or None if there is no element in the array.
+         * Returns optionally the first element of the array or None if the array is empty.
          */
         head : Optional<T>;
 
         /**
          * ES2015 indexOf with Optional support
          */
-        indexOfOpt(value: T) : Optional<number>;
+        indexOfOpt(value: T, fromIndex ?: number) : Optional<number>;
 
         /**
          * Gets all but the last element of array.
          */
         initial : Array<T>;
+
+        /**
+         * Creates an array of unique values that are included in all given arrays using SameValueZero for equality comparisons.
+         * The order and references of result values are determined by the first array.
+         */
+        intersection(...values: Array<Array<T>>) : Array<T>;
+
+        /**
+         * This method is like intersection except that it accepts iteratee which is invoked for each element of each arrays to
+         * generate the criterion by which they're compared.
+         * The order and references of result values are determined by the first array.
+         */
+        intersectionBy(values: Array<Array<T>>, iteratee : ((value : T) => any)|string) : Array<T>;
+
+        /**
+         * This method is like intersection except that it accepts comparator which is invoked to compare elements of arrays.
+         * The order and references of result values are determined by the first array. The comparator is invoked with two arguments: (arrVal, othVal).
+         */
+        intersectionWith(values : Array<Array<T>>, comparator : (value1:T, value2:T) => boolean) : Array<T>;
+
+        /**
+         * Returns true if the array is empty.
+         */
+        isEmpty : boolean;
+
+        /**
+         * Returns optionally the last element of the array or None if the array is empty.
+         */
+        last : Optional<T>;
+
+        /**
+         * ES2015 lastIndexOf with Optional support
+         */
+        lastIndexOfOpt(value: T, fromIndex ?: number) : Optional<number>;
     }
 }
 
@@ -136,12 +162,12 @@ Array.prototype.difference = function(...values : Array<any>) {
     return lodash.difference(this, ...values);
 };
 
-Array.prototype.differenceBy = function(...values : Array<any>) {
-    return lodash.differenceBy(this, ...values);
+Array.prototype.differenceBy = function(values : Array<Array<any>>, iteratee : ((value:any) => any)|string) {
+    return lodash.differenceBy(this, ...values, iteratee);
 };
 
-Array.prototype.differenceWith = function(...values : Array<any>) {
-    return lodash.differenceWith(this, ...values);
+Array.prototype.differenceWith = function(values : Array<Array<any>>, comparator : (value1:any, value2:any) => boolean) {
+    return lodash.differenceWith(this, ...values, comparator);
 };
 
 Array.prototype.drop = function(n : number) {
@@ -191,23 +217,51 @@ Array.prototype.flattenDepth = function(depth : number = 1) {
 };
 
 Object.defineProperty(Array.prototype, 'head', {
-    get: function () { return this.length === 0 ? None : Some(this[0]); },
+    get: function () { return this.isEmpty ? None : Some(this[0]); },
     enumerable: true,
     configurable: true
 });
 
-Array.prototype.indexOfOpt = function(value : any) {
+Array.prototype.indexOfOpt = function(value : any, fromIndex : number = 0) {
     const res = this.indexOf(value);
     return res < 0 ? None : Some(res);
 };
 
 Object.defineProperty(Array.prototype, 'initial', {
     get: function () {
-        return this.filter((value, index) => (index+1) !== this.length);
+        return this.filter((value, index) => (index + 1) !== this.length);
     },
     enumerable: true,
     configurable: true
 });
 
+Array.prototype.intersection = function(...values : Array<Array<any>>) {
+    return lodash.intersection(this, ...values);
+};
+
+Array.prototype.intersectionBy = function(values : Array<Array<any>>, iteratee : ((value : any) => any)|string) {
+    return lodash.intersectionBy(this, ...values, iteratee);
+};
+
+Array.prototype.intersectionWith = function(values : Array<Array<any>>, comparator : (value1:any, value2:any) => boolean) {
+    return lodash.intersectionWith(this, ...values, comparator);
+};
+
+Object.defineProperty(Array.prototype, 'isEmpty', {
+    get: function () { return this.length === 0; },
+    enumerable: true,
+    configurable: true
+});
+
+Object.defineProperty(Array.prototype, 'last', {
+    get: function () { return this.isEmpty ? None : Some(this[this.length - 1]); },
+    enumerable: true,
+    configurable: true
+});
+
+Array.prototype.lastIndexOfOpt = function(value : any, fromIndex ?: number) {
+    const res = this.lastIndexOf(value, fromIndex || this.length - 1);
+    return res === -1 ? None : Some(res);
+};
 
 export default {};
--- test/Array.ts
+++ test/Array.ts
@@ -1,7 +1,7 @@
 import * as assert from 'power-assert';
 import * as lodash from 'lodash';
 
-import "../src/Array";
+import '../src/Array';
 
 
 describe('scalts-array', () => {
@@ -16,10 +16,26 @@ describe('scalts-array', () => {
 
     testLodashMethod('difference', [2, 1], [2, 3]);
 
-    testLodashMethod('differenceBy', [2.1, 1.2], [2.3, 3.4], Math.floor);
-    testLodashMethod('differenceBy', [{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], 'x');
+    it('differenceBy', () => {
+        const array = [2.1, 1.2];
+        const values = [[2.3, 3.4]];
+        const iteratee = Math.floor;
+        assert(lodash.isEqual(array.differenceBy(values, iteratee), lodash.differenceBy(array, ...values, iteratee)))
+    });
 
-    testLodashMethod('differenceWith', [{ 'x': 1, 'y': 2 }, { 'x': 2, 'y': 1 }], [{ 'x': 1, 'y': 2 }], lodash.isEqual);
+    it('differenceBy', () => {
+        const array = [{ 'x': 1 }];
+        const values = [[{ 'x': 2 }, { 'x': 1 }]];
+        const iteratee = 'x';
+        assert(lodash.isEqual(array.differenceBy(values, iteratee), lodash.differenceBy(array, ...values, iteratee)))
+    });
+
+    it('differenceWith', () => {
+        const array = [{ 'x': 1, 'y': 2 }, { 'x': 2, 'y': 1 }];
+        const values = [[{ 'x': 1, 'y': 2 }]];
+        const iteratee = lodash.isEqual;
+        assert(lodash.isEqual(array.differenceWith(values, iteratee), lodash.differenceWith(array, ...values, iteratee)))
+    });
 
     testLodashMethod('drop', [1, 2, 3]);
     testLodashMethod('drop', [1, 2, 3], 2);
@@ -47,27 +63,27 @@ describe('scalts-array', () => {
     testLodashMethod('dropWhile', users, ['active', false]);
     testLodashMethod('dropWhile', users, 'active');
 
-    it("findOpt", () => {
+    it('findOpt', () => {
         assert([1,2,3].findOpt(n => n === 4).isEmpty);
         assert([1,2,3].findOpt(n => n === 2).fold(false, i => i === 2));
     });
 
-    it("findIndexOpt", () => {
+    it('findIndexOpt', () => {
         assert([2,3,1].findIndexOpt(n => n === 4).isEmpty);
         assert([2,3,1].findIndexOpt(n => n === 3).fold(false, i => i === 1));
     });
 
-    it("findLastIndexOpt", () => {
+    it('findLastIndexOpt', () => {
         assert([1,1,1].findLastIndexOpt(n => n === 4).isEmpty);
         assert([1,1,1].findLastIndexOpt(n => n === 1).fold(false, i => i === 2));
     });
 
-    it("flatten", () => {
+    it('flatten', () => {
         const array = [1, [2, [3, [4]], 5]];
         assert(lodash.isEqual(array.flatten, lodash.flatten(array)))
     });
 
-    it("flattenDeep", () => {
+    it('flattenDeep', () => {
         const array = [1, [2, [3, [4]], 5]];
         assert(lodash.isEqual(array.flattenDeep, lodash.flattenDeep(array)));
     });
@@ -75,19 +91,56 @@ describe('scalts-array', () => {
     testLodashMethod('flattenDepth', [1, [2, [3, [4]], 5]], 1);
     testLodashMethod('flattenDepth', [1, [2, [3, [4]], 5]], 2);
 
-    it("head", () => {
+    it('head', () => {
         assert([].head.isEmpty);
         assert([1,2,3].head.fold(false, i => i === 1));
     });
 
-    it("indexOfOpt", () => {
+    it('indexOfOpt', () => {
         assert([].indexOfOpt(1).isEmpty);
         assert([1,2,3].indexOfOpt(2).fold(false, i => i === 1));
     });
 
-
-    it("initial", () => {
+    it('initial', () => {
         const array = [1, 2, 3];
         assert(lodash.isEqual(array.initial, lodash.initial(array)))
+    });
+
+    testLodashMethod('intersection', [2, 1], [2, 3]);
+
+    it('intersectionBy', () => {
+        const array = [2.1, 1.2];
+        const values = [[2.3, 3.4]];
+        const iteratee = Math.floor;
+        assert(lodash.isEqual(array.intersectionBy(values, iteratee), lodash.intersectionBy(array, ...values, iteratee)))
+    });
+
+    it('intersectionBy', () => {
+        const array = [{ 'x': 1 }];
+        const values = [[{ 'x': 2 }, { 'x': 1 }]];
+        const iteratee = 'x';
+        assert(lodash.isEqual(array.intersectionBy(values, iteratee), lodash.intersectionBy(array, ...values, iteratee)))
+    });
+
+    it('intersectionWith', () => {
+        const array = [{ 'x': 1, 'y': 2 }, { 'x': 2, 'y': 1 }];
+        const values = [[{ 'x': 1, 'y': 1 }, { 'x': 1, 'y': 2 }]];
+        const iteratee = lodash.isEqual;
+        assert(lodash.isEqual(array.intersectionWith(values, iteratee), lodash.intersectionWith(array, ...values, iteratee)))
+    });
+
+    it('isEmpty', () => {
+        assert([].isEmpty);
+        assert(![2,3,1].isEmpty);
+    });
+
+    it('last', () => {
+        assert([].last.isEmpty);
+        assert([1,2,3].last.fold(false, i => i === 3));
+    });
+
+    it('lastIndexOfOpt', () => {
+        assert([].lastIndexOfOpt(1).isEmpty);
+        assert([1,2,3,2].lastIndexOfOpt(2).fold(false, i => i === 3));
     });
 });
